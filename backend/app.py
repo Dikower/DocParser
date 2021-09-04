58e70c7c8645006e1d607e98a2e3824728c26845
Din Dmitriy <dmitriy1d01@gmail.com>
2021-09-05 02:22:23 +0300

    Backend works!

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import uuid
 import logging
 import pathlib
 import time
@@ -7,11 +8,13 @@ import httpx
 import uvicorn
 from fastapi import FastAPI, File, HTTPException, UploadFile
 from starlette.middleware.cors import CORSMiddleware
-
+from starlette.staticfiles import StaticFiles
+import pandas as pd
 import pipeline
 
 
 app = FastAPI()
+app.mount('/public', StaticFiles(directory='public'), name='public')
 p = pathlib.Path('files')
 p.mkdir(parents=True, exist_ok=True)
 
@@ -31,12 +34,31 @@ async def register_doc(file_path, nomenclatureId):
         res = await client.post(
             'http://elib-hackathon.psb.netintel.ru/elib/api/service/documents',
             headers={'Authorization': 'Basic ZWxpYi1zdXBlcnVzZXI6MTIz'},
-            data={'createRequest': '{"documentNomenclatureId": "' + nomenclatureId + '"}'},
+            data={'createRequest': '{"documentNomenclatureId": "' + str(nomenclatureId) + '"}'},
             files={'attachments': open(file_path, 'rb')}
         )
         print(f'Registered new doc: {res.text}')
         print(f'Took: {round(time.time() - start, 4)}s')
 
+
+def generate_report(data):
+    table = {}
+    res = data['result']
+    stat = data['stats']
+    table['Название'] = res['name']
+    table['Коды номенклатуры'] = res['code']
+    table['Путь'] = '/'.join(res['path'])
+    table['Список критериев'] = ', '.join(res['criteria_list'])
+    table['Распознавание'] = 'Да' if stat['usedOCR'] else 'Нет'
+    table['Начало'] = stat['startAt']
+    table['Конец'] = stat['endAt']
+    table['Заняло'] = stat['took']
+
+    sr = pd.Series(table)
+    name = f'{uuid.uuid4()}.xlsx'
+    sr.to_excel(f'public/{name}', encoding='utf8')
+    print(f'Report generated: {name}')
+    return f'{name}'
 
 
 @app.post('/upload')
@@ -50,14 +72,15 @@ async def process_file(in_file: UploadFile = File(...)):
         print('Start pipeline')
         result, used_ocr = pipeline.predict(str(p / in_file.filename))
         print(f'Finished pipeline, took: {round(time.time() - start, 4)}s')
-        reg_start = time.time()
         asyncio.create_task(register_doc(p / in_file.filename, result['result']['code']))
         end = time.time()
         result['stats']['startAt'] = start
         result['stats']['endAt'] = end
         result['stats']['took'] = end - start
         result['stats']['usedOCR'] = used_ocr
+        result['report'] = generate_report(result)
         return result
+
     except:
         raise HTTPException(415, 'Wrong file type was passed')
     # finally:
