2e3ec8d9c20008bc9d871cffb31cac99b162b0b9
kuvshinovmark <kuvshinovmark2001@gmail.com>
2021-09-04 11:14:33 +0300

    front_Install

---
+++ frontend/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Router from "svelte-spa-router";
+	import Landing from "./pages/landing.svelte";
+	import Main from "./Pages/Main.svelte";
+	
+	const routes = {
+		"/": Landing,
+		"/main": Main,
+	}
+</script>
+
+<Router {routes} />
+
+
